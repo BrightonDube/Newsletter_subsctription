b7149ae6536b902b870d3528480748c3b40d7486
BrightonDube <BrightonDube@users.noreply.github.com>
2020-03-31 18:04:31 +0200

    add subscription functionality

--- app.js
+++ app.js
@@ -11,14 +11,56 @@ app.get("/", (req, res)=>{
     res.sendFile(`${__dirname}/signup.html`);
 });
 
-app.post("/success.html", (req, res)=>{
-    let name = req.body.name;
-    let email = req.body.email;
 
-    console.log(`${name} ${email}`);
-    res.send();
+app.post("/success", (req, res)=>{
+    try {
+        let name = req.body.name;
+        let email = req.body.email;
+        let data = JSON.stringify({
+            members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_data: {
+                FNAME: name
+            }
+        }]
+        }); //Always convert any POST data to JSON before posting!!
+        (async () => {
+            const options = {
+                url: 'https://us19.api.mailchimp.com/3.0/lists/918954731d',  
+                method: 'POST',
+                headers: //use this for basic authentication
+                    {"Authorization":  "insculpt d651286af8e3c7e26f72b72659443a70-us19"
+                }, 
+                body: data
+            };
+            try {
+                const response = await got(options);
+                console.log(response.body.statusCode);
+                
+            } catch (error) {
+                console.log(error.response.body);
+                
+            }
+        })();
+    
+        console.log(`${name} ${email}`);
+        res.sendFile(`${__dirname}/success.html`);
+    } catch (error) {
+        res.sendFile(`${__dirname}/failure.html`);
+    }
+   
+    
 });
+app.get("/success", (req, res)=>{
+    res.sendFile(`${__dirname}/success.html`);
+});
+app.get("/failure", (req, res)=>{
+    res.sendFile(`${__dirname}/failure.html`);
+});
+
 
 app.listen(port, ()=>{
     console.log("Server listening on port " + port);
 });
+
